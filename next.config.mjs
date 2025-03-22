5038708fc45f199af598f7f11b476bae58645f38
infocommunication <128946685+infocommunication@users.noreply.github.com>
2025-03-22 16:58:47 +0800

    help

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        optimizeCss: true, // 启用 CSS 优化
-    },
-    env: {
-        NEXT_FONT_GOOGLE_IGNORE: "true", // 忽略 Google Fonts
-    },
-    output: "export", // 强制静态导出
-    distDir: "out", // 导出到 `out/` 目录
+    output: 'export',
     images: {
-        unoptimized: true, // 让 Next.js 图片支持 GitHub Pages
+        unoptimized: true,
     },
     reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
